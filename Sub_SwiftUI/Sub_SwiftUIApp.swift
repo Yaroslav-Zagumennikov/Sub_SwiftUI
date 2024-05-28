f4fcb121548a0f504b0201907e5af375ae432aaf
Yaroslav_Zagumennikov <zagumennikov.y@techspaceapp.com>
2024-05-28 11:18:05 +0500

    Initial Commit

---
+++ Sub_SwiftUI/Sub_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sub_SwiftUIApp.swift
+//  Sub_SwiftUI
+//
+//  Created by Yaroslav Zagumennikov on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sub_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
